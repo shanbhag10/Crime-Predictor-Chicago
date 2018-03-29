2976145f11626a3005af92c3b05edf2a5a128e6b
Saurabh Shanbhag <sshanbh2@ncsu.edu>
2018-03-29 03:15:17 -0400

    Create haversine_function.py

---
+++ haversine_function.py
@@ -0,0 +1,14 @@
+def haversine(point1, point2):
+    
+    lat1, lon1 = point1
+    lat2, lon2 = point2
+    radius = 3959 
+
+    dlat = math.radians(lat2-lat1)
+    dlon = math.radians(lon2-lon1)
+    a = math.sin(dlat/2) * math.sin(dlat/2) + math.cos(math.radians(lat1)) \
+        * math.cos(math.radians(lat2)) * math.sin(dlon/2) * math.sin(dlon/2)
+    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1-a))
+    d = radius * c
+
+    return d
