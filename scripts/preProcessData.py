951cdd2c7acb1d3075519c749b343b560901819c
Chirag Jain <csjain@ncsu.edu>
2018-04-11 13:22:49 -0400

    Added out file as param

--- scripts/preProcessData.py
+++ scripts/preProcessData.py
@@ -8,6 +8,7 @@ import sys
 
 def processRawData(paramList):
     """Process the raw files based in the params."""
+    outFiles = []
     for params in paramList:
         inName = params[0]
         outName = params[1]
@@ -23,6 +24,7 @@ def processRawData(paramList):
             rawDataFrame = rawDataFrame.loc[rawDataFrame[colKey] == key]
         rawDataFrame = rawDataFrame[[lat, lon]]
         rawDataFrame.to_csv(outName, header=False, index=False)
+        outFiles.append(outName)
 
 
 def readParams(configFile):
@@ -38,19 +40,22 @@ def readParams(configFile):
     return paramList
 
 
-def generateMainConfigFile():
+def generateMainConfigFile(outFiles, outConfig):
     """Generate the Input file for main algorithm."""
+    with open(outConfig) as outConfigHandle:
+        outConfigHandle.write(','.join(outFiles))
 
 
 def main():
     """Initialize everything and run the algorithm."""
-    if len(sys.argv) < 2:
-        print('Please pass the parameters <CONFIG_FILE>')
+    if len(sys.argv) < 3:
+        print('Please pass the parameters <CONFIG_FILE> <OUT_CONFIG>')
         sys.exit(-1)
     configFile = sys.argv[1]
+    outConfig = sys.argv[2]
     paramList = readParams(configFile)
-    processRawData(paramList)
-    generateMainConfigFile(paramList)
+    outFiles = processRawData(paramList)
+    generateMainConfigFile(outFiles, outConfig)
 
 if __name__ == '__main__':
     main()
